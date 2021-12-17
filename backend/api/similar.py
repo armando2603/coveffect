6d97cc42e89b6126023b3e598293045777ba8042
FrInve <francesco1.invernici@mail.polimi.it>
2021-12-17 16:53:02 +0100

    Remove euclidean similarity

--- backend/api/similar.py
+++ backend/api/similar.py
@@ -21,23 +21,3 @@ def similar_by_cord(cord_uid):
         results.append(Metadata.query.filter(Metadata.annoy_id == paper).first())
     results = [md.serialize() for md in results]
     return results
-
-############# VERSION WITH EUCLIDEAN DISTANCE #####################################
-
-INDEX_FILE_2 = 'api/local_data/annoy/embeddings_euclidean.ann'
-DISTANCE_2 = 'euclidean'
-
-index_2 = annoy.AnnoyIndex(FEATURES,DISTANCE_2)
-index_2.load(INDEX_FILE_2)
-
-def similar_by_cord_euclidean(cord_uid):
-    # Get the complete row
-    original = Metadata.query.filter(Metadata.cord_uid == cord_uid).first()
-    # Get similar papers by annoy_id using the index
-    similar_papers = index_2.get_nns_by_item(original.annoy_id, 11)
-    # Get the complete description for each similar paper
-    results = []
-    for paper in similar_papers[1:]:
-        results.append(Metadata.query.filter(Metadata.annoy_id == paper).first())
-    results = [md.serialize() for md in results]
-    return results
