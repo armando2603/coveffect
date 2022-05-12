1591618e9289ef9eadfbdee0c47dd4296ee3073c
FrInve <francesco1.invernici@mail.polimi.it>
2022-05-13 00:58:55 +0200

    Add database config to docker-compose

--- backend/database/database.py
+++ backend/database/database.py
@@ -4,7 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #CONN_STR = "mariadb+mariadbconnector://retriever:VeryDifficult@geco.gotdns.ch:65500/cord19"
 # CONN_STR = "mysql+pymysql://retriever:VeryDifficult@geco.gotdns.ch:65500/cord19"
-CONN_STR = "mysql+pymysql://retriever:VeryDifficult@muteffstage-db:3306/production"
+#CONN_STR = "mysql+pymysql://retriever:VeryDifficult@muteffstage-db:3306/production"
+CONN_STR = "mysql+pymysql://retriever:VeryDifficult@geco.gotdns.ch:65500/production"
+### docker-compose connection string
+#CONN_STR = "mysql+pymysql://retriever:VeryDifficult@database:3306/production"
 engine = sa.create_engine(CONN_STR)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
